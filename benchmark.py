13d187e6b926b5ae62b92266b4bbac02861f7023
boudjemaa <boudjemaa.bs.bs@gmail.com>
2025-04-29 15:34:17 +0200

    modification benchmark

--- benchmark.py
+++ benchmark.py
@@ -5,16 +5,16 @@ from graphe_V1 import *
 import time
 
 graphes = [
-Graphe(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["C"]}, "A"),
-Graphe(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["A"]}, "A"),
-Graphe(["A", "B", "C"], {"A": ["B", "C"], "B": ["A", "C"], "C": ["A", "B"]}, "A"),
-Graphe(["A", "B", "C"], {"A": ["B"], "B": ["A", "C"], "C": ["B"]}, "A"),
-Graphe(["A", "B", "C"], {"A": ["C"], "B": ["C"], "C": ["C"]}, "A"),
-Graphe(["A", "B", "C"], {"A": ["B"], "B": ["C", "A"], "C": ["A"]}, "A"),
-Graphe(["A", "B"], {"A": ["A", "B"], "B": ["A"]}, "A"),
-Graphe(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["B"]}, "A"),
-Graphe(["A", "B"], {"A": ["A", "B"], "B": ["A", "B"]}, "A"),
-Graphe(["A", "B", "C", "D"], {"A": ["B"], "B": ["C"], "C": ["D"], "D": ["A"]}, "A"),
+Graphe([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 2) % 20}"] for i in range(20)}, "S0"),
+Graphe([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}"] for i in range(20)}, "S0"),
+Graphe([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 2) % 20}"] for i in range(20)}, "S0"),
+Graphe([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 3) % 20}"] for i in range(20)}, "S0"),
+Graphe([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 4) % 20}"] for i in range(20)}, "S0"),
+Graphe([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 5) % 20}"] for i in range(20)}, "S0"),
+Graphe([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 6) % 20}"] for i in range(20)}, "S0"),
+Graphe([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 7) % 20}"] for i in range(20)}, "S0"),
+Graphe([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 8) % 20}"] for i in range(20)}, "S0"),
+Graphe([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 9) % 20}"] for i in range(20)}, "S0"),
 Graphe([f"S{i}" for i in range(20)] + ["A"], {**{f"S{i}": [f"S{(i + 1) % 20}"] for i in range(20)}, "A": ["S0", "S1"]}, "S0")
 ]
 
@@ -22,21 +22,20 @@ methodes = [
     "etat_infini_brute_force",
     "etas_infinis_glouton",
     "get_etats_dans_cycles",
-    "get_etats_multi_cycles",
     "trouver_cycles"
 ]
 
 graphes_3 = [
-Graphe_3(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["C"]}, "A"),
-Graphe_3(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["A"]}, "A"),
-Graphe_3(["A", "B", "C"], {"A": ["B", "C"], "B": ["A", "C"], "C": ["A", "B"]}, "A"),
-Graphe_3(["A", "B", "C"], {"A": ["B"], "B": ["A", "C"], "C": ["B"]}, "A"),
-Graphe_3(["A", "B", "C"], {"A": ["C"], "B": ["C"], "C": ["C"]}, "A"),
-Graphe_3(["A", "B", "C"], {"A": ["B"], "B": ["C", "A"], "C": ["A"]}, "A"),
-Graphe_3(["A", "B"], {"A": ["A", "B"], "B": ["A"]}, "A"),
-Graphe_3(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["B"]}, "A"),
-Graphe_3(["A", "B"], {"A": ["A", "B"], "B": ["A", "B"]}, "A"),
-Graphe_3(["A", "B", "C", "D"], {"A": ["B"], "B": ["C"], "C": ["D"], "D": ["A"]}, "A"),
+Graphe_3([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 2) % 20}"] for i in range(20)}, "S0"),
+Graphe_3([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}"] for i in range(20)}, "S0"),
+Graphe_3([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 2) % 20}"] for i in range(20)}, "S0"),
+Graphe_3([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 3) % 20}"] for i in range(20)}, "S0"),
+Graphe_3([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 4) % 20}"] for i in range(20)}, "S0"),
+Graphe_3([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 5) % 20}"] for i in range(20)}, "S0"),
+Graphe_3([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 6) % 20}"] for i in range(20)}, "S0"),
+Graphe_3([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 7) % 20}"] for i in range(20)}, "S0"),
+Graphe_3([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 8) % 20}"] for i in range(20)}, "S0"),
+Graphe_3([f"S{i}" for i in range(20)], {f"S{i}": [f"S{(i + 1) % 20}", f"S{(i + 9) % 20}"] for i in range(20)}, "S0"),
 Graphe_3([f"S{i}" for i in range(20)] + ["A"], {**{f"S{i}": [f"S{(i + 1) % 20}"] for i in range(20)}, "A": ["S0", "S1"]}, "S0")
 ]
 
@@ -44,46 +43,10 @@ methodes_3 = [
     "etat_infini_brute_force_3",
     "etas_infinis_glouton_3",
     "etats_dans_cycles_3",
-    "etats_multi_cycle_3",
     "trouver_cycles_3"
 ]
 
-graphes_2 = [
-Graphe_2(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["C"]}, "A"),
-Graphe_2(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["A"]}, "A"),
-Graphe_2(["A", "B", "C"], {"A": ["B", "C"], "B": ["A", "C"], "C": ["A", "B"]}, "A"),
-Graphe_2(["A", "B", "C"], {"A": ["B"], "B": ["A", "C"], "C": ["B"]}, "A"),
-Graphe_2(["A", "B", "C"], {"A": ["C"], "B": ["C"], "C": ["C"]}, "A"),
-Graphe_2(["A", "B", "C"], {"A": ["B"], "B": ["C", "A"], "C": ["A"]}, "A"),
-Graphe_2(["A", "B"], {"A": ["A", "B"], "B": ["A"]}, "A"),
-Graphe_2(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["B"]}, "A"),
-Graphe_2(["A", "B"], {"A": ["A", "B"], "B": ["A", "B"]}, "A"),
-Graphe_2(["A", "B", "C", "D"], {"A": ["B"], "B": ["C"], "C": ["D"], "D": ["A"]}, "A"),
-Graphe_2([f"S{i}" for i in range(20)] + ["A"], {**{f"S{i}": [f"S{(i + 1) % 20}"] for i in range(20)}, "A": ["S0", "S1"]}, "S0")
-]
-methodes_2 = [
-    "etat_infini_brute_force_2",
-    "etas_infinis_glouton_2",
-    "trouver_cycles_2"
-]
 
-graphes_1 = [
-Graphe_1(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["C"]}, "A"),
-Graphe_1(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["A"]}, "A"),
-Graphe_1(["A", "B", "C"], {"A": ["B", "C"], "B": ["A", "C"], "C": ["A", "B"]}, "A"),
-Graphe_1(["A", "B", "C"], {"A": ["B"], "B": ["A", "C"], "C": ["B"]}, "A"),
-Graphe_1(["A", "B", "C"], {"A": ["C"], "B": ["C"], "C": ["C"]}, "A"),
-Graphe_1(["A", "B", "C"], {"A": ["B"], "B": ["C", "A"], "C": ["A"]}, "A"),
-Graphe_1(["A", "B"], {"A": ["A", "B"], "B": ["A"]}, "A"),
-Graphe_1(["A", "B", "C"], {"A": ["B"], "B": ["C"], "C": ["B"]}, "A"),
-Graphe_1(["A", "B"], {"A": ["A", "B"], "B": ["A", "B"]}, "A"),
-Graphe_1(["A", "B", "C", "D"], {"A": ["B"], "B": ["C"], "C": ["D"], "D": ["A"]}, "A"),
-Graphe_1([f"S{i}" for i in range(20)] + ["A"], {**{f"S{i}": [f"S{(i + 1) % 20}"] for i in range(20)}, "A": ["S0", "S1"]}, "S0")
-]
-methodes_1 = [
-    "etas_infinis_glouton_1",
-    "trouver_cycles_1"
-]
 
 
 
@@ -111,21 +74,3 @@ for m3 in methodes_3:
     bench(m3, graphes_3)
 print("\n")
 
-
-'''
-print("Version 2")
-for m2 in methodes_2:
-    bench(m2, graphes_2)
-print("\n")
-
-
-
-print("Version 1")
-print("On cherche S'IL Y A un cycle")
-for m1 in methodes_1:
-    if m1=="trouver_cycles_1":
-        bench(m1, graphes_1)
-    else:
-        bench(m1, graphes_1,1)
-print("\n")
-'''
